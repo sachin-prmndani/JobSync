32f2bab535a422de635bb9a0eb950d36389057cb
sachin <sachinkumar59756@gmail.com>
2026-07-14 18:53:00 +0530

    fixed url again

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: 'job-sync-hazel.vercel.app',
+        origin: 'https://job-sync-hazel.vercel.app/',
         credentials: true,
     })
 )
